eec83073e90bd8d08a91ca456ad902249a9a2ded
ALNezar <170512776+ALNezar@users.noreply.github.com>
2025-09-18 23:30:59 +0300

    fix(js): improve score display after each round and added 5 rounds of gameplay

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ console.log (getComputerChoice(1))
 function gamePlay(humanAnswer,computerAnswer){
     
 
-
+   for (let i = 0; i < 5; i++) { 
     humanAnswer = getHumanChoice(humanAnswer);
         let HumanAnswer = humanAnswer;  
     computerAnswer = getComputerChoice(3);
@@ -31,34 +31,54 @@ function gamePlay(humanAnswer,computerAnswer){
 
     if (HumanAnswer === "rock" && computerAnswer === 0 || HumanAnswer === "paper" && computerAnswer === 1 || HumanAnswer === "Scissors" && computerAnswer === 2   ){
         console.log("tie!")
-
+        console.log("Score:" + humanScore)
+        console.log("computer Score:" + computerScore)
     // rock
     } else if (HumanAnswer === "rock" && computerAnswer === 2){
            console.log("Rock beats Siscors! (you win!) +1")
            humanScore++;
 
+        console.log("Score:" + humanScore)
+        console.log("computer Score:" + computerScore)
+
     } else if (HumanAnswer === "rock" && computerAnswer === 1){
            console.log("Paper beats Rock! (you lost) :(")
-           computerScore++;  
+           computerScore++; 
+           
+          console.log("Score:" + humanScore)
+        console.log("computer Score:" + computerScore)
+     
     //paper
     } else if (HumanAnswer === "paper" && computerAnswer === 0){
            console.log("Paper beats Rock! (you win!) +1")
-           humanScore++;
+           humanScore++;   
+            console.log("Score:" + humanScore)
+        console.log("computer Score:" + computerScore)
+
 
     } else if (HumanAnswer === "paper" && computerAnswer === 2){
            console.log("Siscors beats paper! (you lost) :(")
            computerScore++;
-    } //Siscors
+               console.log("Score:" + humanScore)
+        console.log("computer Score:" + computerScore)
+
+    } 
+    //Siscors
     else if (HumanAnswer === "Siscors" && computerAnswer === 1){
            console.log("Siscors beats Paper! (you win!) +1")
            humanScore++; 
-           
+               console.log("Score:" + humanScore)
+        console.log("computer Score:" + computerScore)
+
 
     } else if (HumanAnswer === "Siscors" && computerAnswer === 0){
            console.log("Rock beats Siscors! (you lost) :(")
            computerScore++;
+               console.log("Score:" + humanScore)
+        console.log("computer Score:" + computerScore)
 
     }
+}
 }
 gamePlay()
 
